dd82a7f51945f4a495380b3076ed3635d004efd1
Rafał Giemza <rafal.giemza@softgarden.de>
2024-01-12 00:41:32 +0100

    Add links aka menu on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { authorize, getTask, postAnswer } from "@/utils/api";
+import Link from "next/link";
 
 export default async function Home() {
   const data = await authorize("HelloAPI");
@@ -9,6 +10,11 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>AI DEVS TASKER</h1>
+
+      <Link href={"/C01L01-helloapi"}>C01L01-helloapi</Link>
+      <Link href={"/C01L04-blogger"}>C01L04-blogger</Link>
+      <Link href={"/C01L04-moderation"}>C01L04-moderation</Link>
+      <Link href={"/C01L05-prompt-engineering"}>C01L05-prompt-engineering</Link>
     </main>
   );
 }
