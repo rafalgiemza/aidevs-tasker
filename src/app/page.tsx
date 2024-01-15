96e059a902bbd7a93342e67d5ee1392be967fa8f
Rafał Giemza <rafal.giemza@softgarden.de>
2024-01-15 11:33:56 +0100

    fix link, improve UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>AI DEVS TASKER</h1>
-
+    <main className="flex min-h-screen flex-col items-center gap-5 p-12">
       <Link href={"/C01L01-helloapi"}>C01L01-helloapi</Link>
       <Link href={"/C01L04-blogger"}>C01L04-blogger</Link>
       <Link href={"/C01L04-moderation"}>C01L04-moderation</Link>
